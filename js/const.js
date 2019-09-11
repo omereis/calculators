d3bda2353d78de610000f6f987db250caed35ac1
omereis <omereis@yahoo.com>
2019-09-11 14:46:39 -0400

    added constants const.js

---
+++ js/const.js
@@ -0,0 +1,10 @@
+const ServerCommands = {
+    START_FIT   : 'StartFit',
+    DELETE      : 'Delete',
+    GET_DATA    : 'GetData',
+    GET_STATUS  : 'GetStatus',
+    PRINT_STATUS: 'print_status',
+    GET_RESULTS : 'get_results',
+    REFL1D_FIT  : 'Refl1D_Fit'
+};
+
